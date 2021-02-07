3f3ac058efe616ff7c9e38ef64b3df1d4194611c
d036843 <ch.weiss@sap.com>
2021-02-07 16:17:44 +0100

    upate

--- vue-radio/vue.config.js
+++ vue-radio/vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
   ],
   //https://cli.vuejs.org/guide/deployment.html#github-pages
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/'
+  ? '/vue-radio/dist/'
   : '/',
 
 }
